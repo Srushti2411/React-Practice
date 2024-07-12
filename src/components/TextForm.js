f73ac694645d6568c701cc76f7c65631f2c90a97
Srushti2411 <srushtigarad24@gmail.com>
2024-07-12 18:48:34 +0530

    Enhanced The  Text Converter

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,18 +5,39 @@ function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText)
   }
+  const convertToLowercase = ()=>{
+      let newText = text.toLowerCase();
+      setText(newText)
+    }
+  const clearText = () =>{
+    let newText = ' '
+    setText(newText)
+  }
+
   const hanleOnChange = (event)=>{
         setText(event.target.value)
   }
   const [text,setText] = useState('')
   return (
+    <>
     <div>
     <h1>{props.heading}</h1>
     <div className="mb-3">
     <textarea className="form-control" value={text} onChange={hanleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className='btn btn-primary' onClick={convertToUppercase}>Convert to Uppercase</button>
+    <button className='btn btn-primary mx-2' onClick={convertToUppercase}>Convert to Uppercase</button>
+    <button className='btn btn-primary mx-2' onClick={convertToLowercase}>Convert to Lowercase</button>
+    <button className='btn btn-primary mx-2' onClick={clearText}>Clear text</button>
     </div>
+
+    <div className='container my-3'>
+      <h2 >Text Summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length}Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
 
